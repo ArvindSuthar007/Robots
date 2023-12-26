b464c0f7c77ba43b678371fcb4154a5056cd8afe
Arvind <arvindsutharaasday@gmail.com>
2023-12-26 22:21:37 +0530

    size-adjustments

--- src/card.js
+++ src/card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
     return (
         <>
         <div className="Container">
-            <img alt={props.username} src={`https://robohash.org/${props.name}?set=set4`} width={200} height={200}/>
+            <img alt={props.username} src={`https://robohash.org/${props.name}?set=set4`} />
             <div className="Wrapper">
                 <h2>{props.name}</h2>
                 <h3>{props.mail}</h3>
