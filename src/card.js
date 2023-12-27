c849f6f52e9ca3957202646ad9023b6d9b9ac8cf
Arvind <arvindsutharaasday@gmail.com>
2023-12-27 10:38:35 +0530

    changed alt to id

--- src/card.js
+++ src/card.js
@@ -1,14 +1,14 @@
 import React from "react";
 import './card.css';
 
-const Card = (props) => {
+const Card = ({ id, username, name, mail }) => {
     return (
         <>
         <div className="Container">
-            <img alt={props.username} src={`https://robohash.org/${props.name}?set=set4`} />
+            <img alt={id} src={`https://robohash.org/${username}?set=set4`} />
             <div className="Wrapper">
-                <h2>{props.name}</h2>
-                <h3>{props.mail}</h3>
+                <h2>{name}</h2>
+                <h3>{mail}</h3>
             </div>
         </div>
         </>
