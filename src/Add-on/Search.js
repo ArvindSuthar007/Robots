38f31be0fe5e7c0f1f98f8d0cc87adbbee1872ee
Achal <achaldev3@gmail.com>
2023-12-29 14:46:41 +0530

    fix navigation bar

--- src/Add-on/Search.js
+++ src/Add-on/Search.js
@@ -1,21 +1,25 @@
 import React from "react";
 
 const mystyle = {
-    border: "1px none red",
-    borderRadius: "6px",
-    padding: "0.7em",
-    paddingLeft: "2em",
-    paddingRight: "2em",
-    backgroundColor: "#e6ffff",
-    color: "purple",
-    fontSize: "1.2em",
-    margin: "1.5em 3em 0.5em 1em",
-}
+  border: "1px none red",
+  borderRadius: "6px",
+  padding: "0.7em",
+  paddingLeft: "2em",
+  paddingRight: "2em",
+  backgroundColor: "#e6ffff",
+  color: "purple",
+  fontSize: "1.2em",
+};
 
 const Search = ({ onSearch }) => {
-    return(
-        <input style={mystyle} type="search" placeholder="search here" onChange={onSearch}/>
-    );
-}
+  return (
+    <input
+      style={mystyle}
+      type="search"
+      placeholder="search here"
+      onChange={onSearch}
+    />
+  );
+};
 
 export default Search;
